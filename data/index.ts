9f6cad77e1377327c6bde4787116b8c51c8bcd5e
Khamitkar, Priyank <Priyank.Khamitkar@ddiworld.com>
2024-11-09 14:08:27 -0500

    Updated site to my profile

--- data/index.ts
+++ data/index.ts
@@ -72,66 +72,111 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
+    title: "Energage Platform - HR Tech",
+    des: "Led migration of on-premise applications to Azure Cloud and modernized legacy systems. Spearheaded subscription management services handling $25M+ in revenue, and enhanced QA automation with TestCafe and Selenium.",
+    img: "/energage.svg",
+    iconLists: [
+      "/azure.svg",
+      "/dotnet.svg",
+      "/react.svg",
+      "/sql.svg",
+      "/testcafe.svg",
+    ],
+    link: "https://www.energage.com/platform/",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
+    title: "NetTrust - Cyber Security",
+    des: "Developed a malware detection system for proactive threat mitigation. Integrated Active Directory and LDAP, implemented PDF reporting, and designed key analytics modules for data breach prevention.",
+    img: "/nettrust.svg",
+    iconLists: [
+      "/csharp.svg",
+      "/winforms.svg",
+      "/wcf.svg",
+      "/mysql.svg",
+      "/qualys.svg",
+    ],
+    link: "https://www.taassera.com/",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
+    title: "Buzzinga - Mobile Adventure Puzzle Game",
+    des: "Designed and led the development of a mobile puzzle game, Buzzinga. Created cloud-based messaging for gameplay and optimized Unity3D assets to enhance user interaction on iOS and Android.",
+    img: "/buzzinga.svg",
+    iconLists: [
+      "/unity.svg",
+      "/csharp.svg",
+      "/googleappengine.svg",
+      "/restapi.svg",
+      "/nosql.svg",
+    ],
+    link: "https://apptronixstudios.com/",
   },
 ];
+
+// export const projects = [
+//   {
+//     id: 1,
+//     title: "3D Solar System Planets to Explore",
+//     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+//     img: "/p1.svg",
+//     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+//     link: "/ui.earth.com",
+//   },
+//   {
+//     id: 2,
+//     title: "Yoom - Video Conferencing App",
+//     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+//     img: "/p2.svg",
+//     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+//     link: "/ui.yoom.com",
+//   },
+//   {
+//     id: 3,
+//     title: "AI Image SaaS - Canva Application",
+//     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+//     img: "/p3.svg",
+//     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+//     link: "/ui.aiimg.com",
+//   },
+//   {
+//     id: 4,
+//     title: "Animated Apple Iphone 3D Website",
+//     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+//     img: "/p4.svg",
+//     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+//     link: "/ui.apple.com",
+//   },
+// ];
 
 export const testimonials = [
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Priyank was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Priyank's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Priyank is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Priyank was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Priyank's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Priyank is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Priyank was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Priyank's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Priyank is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Priyank was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Priyank's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Priyank is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Priyank was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Priyank's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Priyank is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
@@ -173,31 +218,35 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Senior Technical Project Manager",
+    company: "Harbinger Group",
+    desc: "Managed multiple large-scale projects, leading cross-functional teams of over 30 members, modernizing applications by migrating to Azure Cloud and implementing agile methodologies for optimized project delivery.",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
-  },
-  {
-    id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
-    id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
+    id: 2,
+    title: "Project Lead - Cyber Security",
+    company: "Senior Technical Project Manager",
+    desc: "Led a malware detection project for NetTrust, developing a system to identify attacks and implementing features such as analytics and PDF report generation. Recognized with the 'Excellence in Project Leadership' award.",
+    className: "md:col-span-2",
     thumbnail: "/exp3.svg",
   },
   {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    id: 3,
+    title: "Technical Lead - Game Development",
+    company: "Apptronix Studios",
+    desc: "Spearheaded development of the mobile adventure puzzle game 'Buzzinga,' overseeing design and Unity3D development, integrating cloud-based middleware, and optimizing touch-based inputs for iOS and Android.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
+  },
+  {
+    id: 4,
+    title: "Software Engineer",
+    company: "Blue Shores Solutions Pvt. Ltd.",
+    desc: "Developed and maintained robust applications using .NET, C#, and SQL, ensuring scalability and performance for enterprise-level solutions.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
   },
 ];
 
